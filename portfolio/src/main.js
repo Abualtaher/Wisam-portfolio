bc125fc89cf48fd2c28e976fd622881ff99d0663
Abualtaher <wisamaltaher@gmail.com>
2025-01-30 22:43:21 +0100

    finished tailwind css instaling

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,7 +1,26 @@
+// Import Vue and the necessary functions
 import { createApp } from "vue";
-import router from "./routes";
-
-import "./style.css";
+import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
+import "./style.css";
 
+// Import route components
+const Home = () => import("./views/Home.vue");
+const About = () => import("./views/About.vue");
+const Movies = () => import("./views/Movies.vue");
+
+// Define routes
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+  { path: "/movies", component: Movies },
+];
+
+// Create router instance
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// Create and mount the Vue application
 createApp(App).use(router).mount("#app");
