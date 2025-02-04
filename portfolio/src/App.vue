76cc603a8e66c9c727797277f233995432225cb2
Abualtaher <wisamaltaher@gmail.com>
2025-02-04 07:10:36 +0100

    trying sections

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
+import Movies from "./views/Movies.vue";
+import About from "./views/About.vue";
 </script>
 <template>
   <Navbar />
+  <About />
+  <Movies />
   <div class="max-w-7xl mx-auto">
     <RouterView />
   </div>
