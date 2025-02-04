1d23599d72adca811ad481a38544d1e3cf7664c5
Abualtaher <wisamaltaher@gmail.com>
2025-02-04 23:02:04 +0100

    new Start

--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,5 +1,8 @@
-<script setup></script>
+<script setup>
+import AboutMe from "../components/AboutMe.vue";
+</script>
 <template>
+  <AboutMe />
   <h1 class="pt-20 dark:text-white">Home</h1>
 </template>
 <style scoped></style>
