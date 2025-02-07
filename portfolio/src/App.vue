df27a041693954f5a4396e61ad7cb660237e0934
Abualtaher <wisamaltaher@gmail.com>
2025-02-07 22:09:55 +0100

    adding Aos

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init({ duration: 1000 });
 </script>
 <template>
   <div class="max-w-7xl mx-auto">
--- portfolio/src/components/HeroSection.vue
+++ portfolio/src/components/HeroSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mt-35 relative w-full" data-aos="zoom-in-up">
+  <section class="mt-35 relative w-full" data-aos="flip-up">
     <div class="absolute top-0 inset-x-0 h-64 flex items-start">
       <div
         class="h-24 w-2/3 bg-gradient-to-br from-[#570cac] blur-2xl invisible opacity-40"
@@ -117,8 +117,4 @@
     </div>
   </section>
 </template>
-<script setup>
-import AOS from "aos";
-import "aos/dist/aos.css";
-AOS.init();
-</script>
+<script setup></script>
