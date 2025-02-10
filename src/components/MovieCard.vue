f0de8fdb44786698adaa9a9e86383f084ffa5183
Abualtaher <wisamaltaher@gmail.com>
2025-02-10 22:35:43 +0100

    testing with uppdating with netlyf

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -20,12 +20,7 @@ const props = defineProps({
           {{ movie.title }}
         </h5>
       </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore amet
-        voluptatem, tenetur rem eos nesciunt. Laborum, deleniti tempora. Optio
-        inventore animi enim, pariatur excepturi suscipit voluptatibus vitae
-        atque commodi aperiam?
-      </p>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Wisam</p>
       <a
         href="#"
         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
