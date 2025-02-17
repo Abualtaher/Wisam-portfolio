e8fb9425eaacec3afc1d4931b8d95dec589ab775
Abualtaher <wisamaltaher@gmail.com>
2025-02-17 12:49:03 +0100

    fixing som bugs with the routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-// Import route components
+// // Import route components
+// const Home = () => import("../views/Home.vue");
+// const About = () => import("../views/About.vue");
+// const Movies = () => import("../views/Movies.vue");
+// const MovieDetails = () => import("../views/MovieDetails.vue");
+
 const Home = () => import("../views/Home.vue");
-const About = () => import("../views/About.vue");
-const Movies = () => import("../views/Movies.vue");
-const MovieDetails = () => import("../views/MovieDetails.vue");
 
 // Define routes
 const routes = [
   { path: "/", component: Home },
-  { path: "/about", component: About },
-  { path: "/movies", component: Movies },
-  { path: "/movies/:id", component: MovieDetails },
+  { path: "/about", component: () => import("../views/About.vue") },
+  { path: "/movies", component: () => import("../views/Movies.vue") },
+  { path: "/movies/:id", component: () => import("../views/MovieDetails.vue") },
 ];
 
 // Create router instance
--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -1,5 +1,37 @@
 <script setup></script>
 <template>
-  <div></div>
+  <div class="dark:text-white">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eius soluta
+    voluptas repudiandae veniam in, nisi exercitationem nulla. Alias, error vero
+    molestias laborum possimus ipsa dolores dicta itaque assumenda magni? Iste
+    ullam odio vero quis itaque ipsa incidunt nemo aspernatur numquam possimus
+    iure ut, doloribus accusantium harum non quaerat ipsam eum impedit? Facilis
+    expedita inventore molestiae officia quia aperiam earum! Ducimus ipsa libero
+    quam ex obcaecati pariatur, dolor debitis aspernatur quo earum hic nulla eum
+    in doloribus at rem omnis maiores aliquam non minima porro tempora aperiam
+    reprehenderit similique. Temporibus? Facere architecto, tenetur saepe
+    laudantium enim perspiciatis necessitatibus omnis placeat similique rerum
+    nobis odit nihil dolores voluptas quod! Corrupti officiis excepturi illum
+    repudiandae hic, ullam reiciendis fuga quaerat cum quis. Atque esse, velit
+    nam unde aliquam a. Error totam quam sapiente eius aperiam tempore ea
+    repellendus, fuga voluptatem accusantium ut corporis voluptatum tenetur quis
+    dicta! Odit eligendi modi quas rem! Fugiat distinctio ab at! Iusto veniam
+    porro quas, quos reprehenderit voluptatem eaque ut doloremque perferendis
+    nam id rem culpa impedit, a tempora illo? Quam ab praesentium blanditiis in
+    nam culpa! Nesciunt quae quo dolorem dolore animi, illum adipisci. Quos
+    reprehenderit accusantium voluptates eum, ut ex itaque! Repudiandae
+    obcaecati ipsam pariatur voluptate similique tempora consequatur magnam
+    suscipit atque culpa. Libero, neque. Inventore laboriosam velit quam
+    molestias consectetur harum omnis vitae, dolor veniam eum dolores, odit
+    facere? Necessitatibus explicabo facilis, officia vitae commodi doloremque
+    quas rem voluptatibus tempore, ipsa quidem quos consequuntur. Repudiandae
+    molestiae minus cupiditate nisi voluptatibus, ducimus ratione earum quae
+    laudantium officiis beatae commodi totam, ipsa accusamus corporis ipsum,
+    maxime reiciendis sit aliquam ex? Rerum incidunt deleniti adipisci voluptate
+    temporibus. Ea eveniet atque harum, aliquid aliquam pariatur odit voluptatum
+    molestias quidem exercitationem libero aspernatur hic molestiae alias
+    accusantium commodi unde aut qui magnam dolore veritatis iste quibusdam.
+    Pariatur, porro iusto.
+  </div>
 </template>
 <style scoped></style>
