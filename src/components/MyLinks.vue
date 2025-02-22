1df03618faa3dc1b534233bf5bbca635a94831fe
Abualtaher <wisamaltaher@gmail.com>
2025-02-22 09:01:50 +0100

    adding text to my sub apps

--- src/components/MyLinks.vue
+++ src/components/MyLinks.vue
@@ -35,11 +35,10 @@
         <h5
           class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
         >
-          Noteworthy technology acquisitions 2021
+          Poker Game
         </h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          This app is made with vanilla HTML CSS JavaScript.
         </p>
       </div>
     </a>
@@ -60,11 +59,10 @@
         <h5
           class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
         >
-          Noteworthy technology acquisitions 2021
+          People Counter
         </h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          This app is made with vanilla HTML CSS JavaScript
         </p>
       </div>
     </a>
