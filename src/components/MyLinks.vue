548da810673ca2b6950ffb586078a22e7ec950f3
Abualtaher <wisamaltaher@gmail.com>
2025-02-24 21:59:11 +0100

    added new game to myLinks

--- src/components/MyLinks.vue
+++ src/components/MyLinks.vue
@@ -67,5 +67,29 @@
       </div>
     </a>
   </div>
+  <div class="flex justify-center items-center" data-aos="fade-up">
+    <a
+      href="https://adorable-jalebi-25e472.netlify.app/"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="m-10 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+    >
+      <img
+        class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+        src="/src/assets/imges/rock-paper-scissors.jpg"
+        alt=""
+      />
+      <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          Rock-Paper-Scissors
+        </h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          This app is made with vanilla HTML CSS JavaScript
+        </p>
+      </div>
+    </a>
+  </div>
 </template>
 <style scoped></style>
