75745146e2172805ceb289042e8744c28de8426b
abualtaher <wisamaltaher@gmail.com>
2026-09-03 17:44:30 +0200

    replace CV file and add To-Do-List project link to MyLinks component

--- src/components/MyLinks.vue
+++ src/components/MyLinks.vue
@@ -20,6 +20,30 @@
   </p>
   <div class="flex justify-center items-center" data-aos="fade-up">
     <a
+      href="https://pagepilot-cms-1.onrender.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="m-10 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+    >
+      <img
+        class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+        src="/src/assets/imges/Todolist.jpg"
+        alt=""
+      />
+      <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          To-Do-List
+        </h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          This app is made with React and it is my first.
+        </p>
+      </div>
+    </a>
+  </div>
+  <div class="flex justify-center items-center" data-aos="fade-up">
+    <a
       href="https://ornate-cactus-79fff9.netlify.app/"
       target="_blank"
       rel="noopener noreferrer"
